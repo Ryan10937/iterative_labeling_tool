e480c9590f754601c3b59d7b4dd1e80e7528c551
Ryan Sterzenbach <ryan@sterzenbach.com>
2024-09-25 18:14:54 -0500

    Reorganized script and pushed buttons to sidebar

--- iterative_labeling.py
+++ iterative_labeling.py
@@ -5,38 +5,50 @@ from utils.streamlit_functs import *
 from utils.relabeling_functs import *
 import sys
 import os
-# sys.path.append(os.path.join(os.getcwd(),'Dataset_Generator/utils'))
 from Dataset_Generator.entities import DatasetGenerator
 if __name__ == '__main__':
+    st.title('Hello Streamlit')
+
+    #####################################################################
+    #################### Initialize values ##############################
     if len(st.session_state.keys()) == 0:
         init_session_values()
         get_next_image()
+    #####################################################################
     
     
-    st.title('Hello Streamlit')
 
-    if st.button('Clear'):
-        st.session_state['coords']=[]
-        st.session_state['loaded_image']=Image.open(st.session_state['current_image-label_pair'].media_path)
+
+    #####################################################################
+    ########################### Buttons #################################
+    with st.sidebar:
+        if st.button('Next Image'):
+            get_next_image()
+
+        if st.button('Submit Edit label'):
+            coord_list_to_fashion_xml(st.session_state['current_image-label_pair'],st.session_state['coords'])
+
+        if st.button('Clear'):
+            st.session_state['coords']=[]
+            st.session_state['loaded_image']=Image.open(st.session_state['current_image-label_pair'].media_path)
+    #####################################################################
     
-    if st.button('Next Image'):
-        get_next_image()
+    
+
+    #####################################################################
+    ######################### Label Coords ##############################
     coords=streamlit_image_coordinates(st.session_state['loaded_image'])
-    # print('coords:',coords)
     if coords != None and coords not in st.session_state['coords']:
         st.session_state['coords'].append(coords)
         draw_pts_on_image()
-    # print(st.session_state["coords"])
 
     if 'coords' in st.session_state:
         if len(st.session_state['coords']) >= 4:
-            #show coords sub-image
-            print(st.session_state['coords'])
             st.image(segment_image_with_coords(st.session_state['current_image-label_pair'].media_path,st.session_state['coords']))
     if st.session_state['current_image-label_pair'].value != None:
         st.text(st.session_state['current_image-label_pair'].value)
+    #####################################################################
 
-    if st.button('edit_label'):
-        coord_list_to_fashion_xml(st.session_state['current_image-label_pair'],st.session_state['coords'])
+    
 
     
